0919cbf93a22cbea7efddf8adef4fc4c93c99b99
Filip Lindgren <filip.k.lindgren@gmail.com>
2021-02-25 11:03:09 +0100

    Testing to push initial project

---
+++ Portfolio/Models/ItemModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    public class ItemModel
+    {
+        public string Id { get; set; }
+        public string location { get; set; }
+        public string comment { get; set; }
+        public ProductModel productType = new ProductModel();
+        public int productTypeIndex { get; set; }
+        public bool selected { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return productType.name;
+        //}
+    }
+}
---
+++ Portfolio/Models/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    public class ProductModel
+    {
+		public string category { get; set; }
+		public string name { get; set; }
+		public string image { get; set; }
+		public string activation { get; set; }
+		public bool selected { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return name;
+        //}
+    }
+}
